d56475f816895de25d88e1195cdc388a82a18d78
Simon-Olivier Duguay <simonkirbi@hotmail.com>
2024-08-06 16:59:24 +0000

    Feature/text widget

--- Widgets/DataWidget.py
+++ Widgets/DataWidget.py
@@ -3,15 +3,17 @@ from PySide6.QtWidgets import QLabel
 from PySide6.QtCore import Qt
 
 class DataWidget(Widget):
-    def __init__(self, parent, position="vertical"):
+    def __init__(self, parent, position="vertical", mode="normal"):
         super(DataWidget, self).__init__(parent)
         self.dataLabel = QLabel("Not initialised")
         self.layout.addWidget(self.dataLabel,alignment=Qt.AlignCenter)
         #self.setStyleSheet("border: 1px solid black;")
+        self.mode = mode
         self.dataValue = "No value"
         self.label = "No label"
         self.unit = ""
         self.round = 2
+        self.maxValue = 0
         self.requiredData = []
         self.delimiter = "\n" if position == "vertical" else " : "
         self.updateDataLabel()
@@ -31,11 +33,18 @@ class DataWidget(Widget):
     def updateDataLabel(self):
         self.dataLabel.setText(self.label + self.delimiter + self.dataValue + " "+ self.unit)
 
+    def processData(self, data):
+        if self.mode == "normal" or self.mode == None:
+            self.dataValue = str(round(data.value,self.round))
+        elif self.mode == "max":
+            if data.value > self.maxValue:
+                self.maxValue = data.value
+                self.dataValue = str(round(self.maxValue,self.round))
+        self.updateDataLabel()
+
     # Here we are receiving measurements wanted in requiredData. 
     # We need to separate them with the value of the source.
     def setData(self, data):
         for i in self.requiredData:
             if data.source == i:
-                self.dataValue = str(round(data.value,2))
-                # Update the data label when recieving new data
-                self.updateDataLabel()
+                self.processData(data)
