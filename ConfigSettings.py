b30081eca3f9cbfa9a715a276b6b7fc6501aece4
soleil anker-baril <soleilankerbaril@gmail.com>
2024-09-11 13:42:18 -0400

    Bug FIx

--- ConfigSettings.py
+++ ConfigSettings.py
@@ -1,9 +1,9 @@
 class ConfigSettings:
     def __init__(self, parent):
         self.app = parent
-        self.serialPort = "COM3"
+        self.serialPort = "COM4"
         self.baudrate = 115200
         self.saveData = False
-        self.configFile = "config.json"
+        self.configFile = "configTestBench.json"
         self.defaultSavePath = "saved_data/cutepylog.csv"
         self.fakeData = False
