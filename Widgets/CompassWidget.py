249c5f89932a769c2c1d8003481af3b58efb5085
Martin Grangé <martin.grange.1@ens.etsmtl.ca>
2024-10-13 23:29:31 +0200

    Adaptation du widget afin qu'il soit strucuté de la meme manière que les autres

--- Widgets/CompassWidget.py
+++ Widgets/CompassWidget.py
@@ -1,9 +1,8 @@
 from Widgets.Widget import Widget
 from PySide6 import QtGui
-from PySide6.QtGui import QPen, QBrush, QPixmap
-from PySide6.QtWidgets import  QGraphicsScene, QGraphicsEllipseItem, QGraphicsView, QGraphicsPixmapItem
-from PySide6.QtCore import QPointF, QRectF, Qt
-from PySide6.QtCore import Signal
+from PySide6.QtGui import QPen, QBrush, QPixmap, QPainter
+from PySide6.QtWidgets import  QGraphicsScene, QGraphicsEllipseItem, QGraphicsView, QGraphicsPixmapItem, QWidget, QVBoxLayout
+from PySide6.QtCore import QPointF, QRect, Qt
 import math
 
 
@@ -14,19 +13,37 @@ from Modules.Tools.mapAPI import mapApi, check_internet
 from Modules.Tools.geo import bearing, distance
 
 class CompassWidget(Widget):
-    updateCompass = Signal()
 
     def __init__(self,parent):
-        super().__init__(parent)
+        super(CompassWidget, self).__init__(parent)
 
-        self.setWindowTitle("Compass Widget")
+        self.verif = 0
+        # Initialize the layout
+        self.vlayout = QVBoxLayout(self)
+        self.drawCompassWidget =  DrawCompassWidget(self)
+        self.vlayout.addWidget(self.drawCompassWidget)
+        self.layout.addLayout(self.vlayout)
+        
+        
 
-        #Création de la scene
-        self.compass = QGraphicsScene(self)     
+        self.requiredData = ["rockets.anirniq.communication.gnss.lat", "rockets.anirniq.communication.gnss.lon"]
 
-        #Couleur de fond
-        self.compass.setBackgroundBrush(QBrush(QtGui.QColor(229, 229, 229)))
 
+    def setData(self, data):
+        if data.source == self.requiredData[0]:
+            self.drawCompassWidget.setLatRocket(data.value)
+            self.verif = self.verif + 1
+        if data.source == self.requiredData[1]:
+            self.drawCompassWidget.setLonRocket(data.value)
+            self.verif = self.verif + 1
+        if self.verif == 2:
+                self.verif = 0
+                self.update()
+        
+
+class DrawCompassWidget(QWidget):
+    def __init__(self, parent=None):
+        super(DrawCompassWidget, self).__init__(parent)
         #Initialisation des constantes
         self.center = QPointF(0,0)
         self.radius = 58
@@ -36,7 +53,7 @@ class CompassWidget(Widget):
         #Initialisation des variables
         self.angle = 0
         self.distance = 0
-        self.verif = 0
+        
 
         #Latitude et longitude de la ground station en degré
         self.latGS = 45.539653
@@ -52,27 +69,25 @@ class CompassWidget(Widget):
 
         # Chargement de l'image de fond
         self.background_image = 0 #Image.open('Resources/map.png')
-
-        #Création du compas
-        self.drawBackground()
-        self.drawCompass()
-        
-
-        #Création de la vue
-        self.view = QGraphicsView(self.compass)
-        self.view.setStyleSheet("border: none;")
-
-        #Ajout du compas au layout 
-        self.layout.addWidget(self.view)
-        
-        self.requiredData = ["rockets.anirniq.communication.gnss.lat", "rockets.anirniq.communication.gnss.lon"]
-
-        # Connecter le signal au slot 
-        self.updateCompass.connect(self.updateCompassSlot)
-        self.updateCompass.emit() 
-
     
-    def drawBackground(self):
+    def setLatRocket(self,latRocket_):
+        self.latRocket = latRocket_
+    
+    def setLonRocket(self,lonRocket_):
+        self.lonRocket = lonRocket_
+
+        
+    def paintEvent(self, event=None):
+        width = self.width()
+        height = self.height()
+
+        # Dessiner la jauge
+        painter = QPainter(self)
+        # Dessiner le fond de la jauge
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(Qt.black, 1))
+        painter.drawEllipse(1, 1, height-2, width-2)
+
         #On test si les coordoonées de la ground sation ont changé. 
         #Si oui on met à jour la carte en fond, sinon on ne fait rien
         #Cela permet de réduire le nombre de demande à l'API
@@ -91,55 +106,33 @@ class CompassWidget(Widget):
             compass_mask(self.background_image)
             #Convertion dans le bon format
             self.background_image = QPixmap("Resources/image_compass_mask.png")
-            self.background_item = QGraphicsPixmapItem(self.background_image)
-            #Positionnement de la map
-            self.background_item.setPos(self.center.x() - self.radius, self.center.y() - self.radius)
-            self.background_item.setScale(2 * self.radius / self.background_image.width())
-            # Ajout de l'élément pour l'image de fond à la scène
-            self.compass.addItem(self.background_item)
+
         else:
             pass
-    
-    def drawCompass(self):
-        #Ajout du cercle
-        self.circle = QGraphicsEllipseItem(
-            QRectF(self.center.x()-self.radius, self.center.y()-self.radius, 2*self.radius, 2*self.radius) 
-        )
-        self.circle.setPen(QPen(Qt.black))
-        self.compass.addItem(self.circle)
+        resized_image = self.background_image.scaled(width - 2, height - 2, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        target_rect = QRect(1, 1, width - 2, height - 2)  # Utiliser QRect pour spécifier la zone
+        painter.drawPixmap(target_rect, resized_image)  # Correctement appelé avec QRect et QPixmap
 
-        #Ajout de la target
-        self.compass.addLine(self.center.x(),self.radius,self.center.x(),-self.radius)
-        self.compass.addLine(-self.radius,self.center.y(),self.radius,self.center.y())
+        painter.drawLine(1, (height-2)/2 + 1, width - 1,(height-2)/2 + 1)
+        painter.drawLine((width-2)/2 + 1, 1, (width-2)/2 + 1, height - 1)
 
-        #Ajout de la bille
-        self.bille = QGraphicsEllipseItem(
-            QRectF(-self.radiusBille,-self.radiusBille, 2*self.radiusBille, 2*self.radiusBille)
-        )
-        self.bille.setBrush(QBrush(Qt.red))
-        self.compass.addItem(self.bille)
-    
-    def setData(self, data):
-        if data.source == self.requiredData[0]:
-            self.latRocket = data.value
-            self.verif = self.verif + 1
-        if data.source == self.requiredData[1]:
-            self.lonRocket = data.value
-            self.verif = self.verif + 1
-        if self.verif == 2:
-                self.verif = 0
-                self.updateCompass.emit()   #On émet le signal
-   
-   #Méthode d'uptade du compas
-    def updateCompassSlot(self):            #Si un signal est émit
+        
+
         self.angle = bearing(self.latGS,self.lonGS,self.latRocket,self.lonRocket)       #Calcul du bearing
         self.distance = distance(self.latGS,self.lonGS,self.latRocket,self.lonRocket)   #Calcul de la distance horizontale à la GS
         if self.distance <= self.range:
-            x = self.center.x() + self.distance/self.range * self.radius * math.cos(math.radians(self.angle-90))
-            y = self.center.y() + self.distance/self.range * self.radius * math.sin(math.radians(self.angle-90))
+            x = ((height-2)/2 +1) + self.distance/self.range * (height-2)/2 * math.cos(math.radians(self.angle-90))
+            y = ((height-2)/2 +1) + self.distance/self.range * (height-2)/2 * math.sin(math.radians(self.angle-90))
         else: 
-            x = self.center.x() + self.radius * math.cos(math.radians(self.angle-90))
-            y = self.center.y() + self.radius * math.sin(math.radians(self.angle-90))
+            x = ((height-2)/2 +1) + (height-2)/2 * math.cos(math.radians(self.angle-90))
+            y = ((height-2)/2 +1) + (height-2)/2 * math.sin(math.radians(self.angle-90))
+
+        # Dessiner la bille (ellipse) en rouge et remplie
+        painter.setBrush(QBrush(Qt.red))  # Remplir avec une couleur rouge
+        painter.setPen(Qt.NoPen)  # Enlever le contour pour la bille
+        painter.drawEllipse(x -self.radiusBille , y - self.radiusBille, 2*self.radiusBille, 2*self.radiusBille)
         
-        self.bille.setRect(x-self.radiusBille, y-self.radiusBille, 2*self.radiusBille, 2*self.radiusBille)
+        painter.end()
+
+        
 
